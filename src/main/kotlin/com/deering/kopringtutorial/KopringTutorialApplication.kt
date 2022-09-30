b7f279b43f37e7e5832540ca4039d75d9ca985bc
deocksoo <deocks@deering.co>
2022-09-30 20:56:33 +0900

    init

---
+++ src/main/kotlin/com/deering/kopringtutorial/KopringTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.deering.kopringtutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/deering/kopringtutorial/KopringTutorialApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.deering.kopringtutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
